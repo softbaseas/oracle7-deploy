6fd8e87c7cdb3bc084b5571398bdeac1f2a1c2d8
Anders W. Olsen <anders@wiberg.tech>
2017-06-07 12:46:47 +0200

    network dns

--- includes/linux/setup_network.sh
+++ includes/linux/setup_network.sh
@@ -4,12 +4,16 @@ hostname=$1
 external=$2;
 internal=$3;
 
+read -p "Gateway ip: " gateway
+read -p "DNS1: " dns1
+read -p "DNS2: " dns2
+
 echo "Hostname = $hostname"
 echo "External = $external"
 echo "Internal = $internal"
 
 nmcli g hostname $fqdn
-nmcli con mod eth0 ipv4.addr $external ipv4.gateway 10.174.234.1
+nmcli con mod eth0 ipv4.addr $external ipv4.gateway $gateway ipv4.dns "$dns1 $dns2"
 nmcli con mod eth1 ipv4.addr $internal
 
 hostname $fqdn;
