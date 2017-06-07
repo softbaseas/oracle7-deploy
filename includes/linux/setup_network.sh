14c1dc6fb41b3478f50d75ea4d49b37801ba86eb
Anders W. Olsen <anders@wiberg.tech>
2017-06-07 10:58:02 +0200

    moving files

---
+++ includes/linux/setup_network.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+hostname=$1
+external=$2;
+internal=$3;
+
+echo "Hostname = $hostname"
+echo "External = $external"
+echo "Internal = $internal"
+
+nmcli g hostname $fqdn
+nmcli con mod eth0 ipv4.addr $external ipv4.gateway 10.174.234.1
+nmcli con mod eth1 ipv4.addr $internal
+
+echo "Network has been changed."
+
+echo;
+
+echo "Changing hosts file..."
+./includes/change_hosts.sh "$1" "$ip"
