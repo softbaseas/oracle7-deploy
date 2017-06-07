d23dfac873fcafda8066e3d62ac279b2d454183a
Anders W. Olsen <anders@wiberg.tech>
2017-06-07 11:06:26 +0200

    add adminserver startup using nodemanager

--- includes/main.sh
+++ includes/main.sh
@@ -6,8 +6,9 @@ echo "";
 
 echo "Choose one of the following possibilities:"
 echo " 1 - Setup Network"
-#echo " 2 - Change Hosts Files"
-#echo " 3 - Change Formsweb"
+echo " 2 - Change Hosts Files"
+echo " 3 - Change Formsweb"
+echo " 4 - Start Admin Server using Nodemanager"
 #echo " 4 - Change Oracle vHosts"
 #echo " 5 - Configure ORDS"
 #echo " 6 - Generate SSL Certificates using Lets Encrypt"
@@ -19,11 +20,19 @@ echo " q - Exit"
 while true; do
     read -p "Choice: " choice
     case $choice in
-      [1]*)
+      [1]*) # Setup network
         if [ -z "$fqdn" ]; then getFQDN; fi
         if [ -z "$ip" ]; then getIP; fi
-        echo "sub=$subdomain";;
-        #./includes/linux/setup_network.sh $fqdn $internalIP $externalIP;;
+        ./includes/linux/setup_network.sh $fqdn $internalIP $externalIP;;
+      [2]*) # Setup /etc/hosts
+        if [ -z "$fqdn" ]; then getFQDN; fi
+        if [ -z "$ip" ]; then getIP; fi
+        ./includes/linux/setup_hosts.sh $fqdn $subdomain $externalIP;;
+      [3]*) # Configure formsweb
+        if [ -z "$fqdn" ]; then getFQDN; fi
+        ./includes/oracle/formsweb.sh $fqdn;;
+      [4]*) # Start adminserver using nodemanager
+        ./includes/oracle/nodemanager.sh ;;
       [q]*) echo "Exiting"; break;;
     esac
 done
---
+++ includes/oracle/nodemanager.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Enter nodemanager username: " username
+read -p "Enter nodemanager password: " password
+
+su - oracle /home/oracle/Oracle/Middleware12c/ofr1/oracle_common/common/bin/wlst.sh << EOF > /tmp/nodemanager.log
+nmConnect($username,$password,'localhost','5556','SBErpc12','/home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12')
+prps = makePropertiesObject("AdminURL=http://ottos12.softbase.ch;7001;Username=$username;Password=$password;weblogic.ListenPort=7001")
+nmStart("AdminServer", props=prps)
+exit()
+EOF
+
+echo "Nodemanager configured";
