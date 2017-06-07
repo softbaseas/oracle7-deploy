ee0d68d519c75357fc47b65f9d1a702bb0516ebd
Anders W. Olsen <anders@wiberg.tech>
2017-06-07 10:48:01 +0200

    sbs install environment

---
+++ includes/oracle/sbs-environment.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export ORACLE_INSTALL_USER=oracle
+export ORACLE_INSTALL_GROUP=oinstall
+export DOMAIN_CONFIGURATION_HOME=/home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12
+export NODE_MANAGER_HOME=/home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12/nodemanager
