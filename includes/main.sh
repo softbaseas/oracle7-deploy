30ff898702f3bdd67218c8bfd4e390444c55936a
Anders W. Olsen <anders@wiberg.tech>
2017-06-07 12:14:33 +0200

    add space

--- includes/main.sh
+++ includes/main.sh
@@ -19,34 +19,34 @@ while true; do
     echo " q - Exit"
     read -p "Choice: " choice
     case $choice in
-      [1]*) # Setup network
+      [1]* ) # Setup network
         if [ -z "$fqdn" ]; then getFQDN; fi
         if [ -z "$ip" ]; then getIP; fi
         ./includes/linux/setup_network.sh $fqdn $internalIP $externalIP;;
-      [2]*) # Setup /etc/hosts
+      [2]* ) # Setup /etc/hosts
         if [ -z "$fqdn" ]; then getFQDN; fi
         if [ -z "$ip" ]; then getIP; fi
         ./includes/linux/setup_hosts.sh $fqdn $subdomain $externalIP;;
-      [3]*) # Configure formsweb
+      [3]* ) # Configure formsweb
         if [ -z "$fqdn" ]; then getFQDN; fi
         ./includes/oracle/formsweb.sh $fqdn;;
-      [4]*) # Change Oracle vhosts
+      [4]* ) # Change Oracle vhosts
         if [ -z "$fqdn" ]; then getFQDN; fi
         ./includes/oracle/vhosts.sh $fqdn;
-      [5]*) # configure ords
+      [5]* ) # configure ords
         ./includes/oracle/ords.sh;;
-      [6]*) # Generate SSL Certificates using Lets Encrypt
+      [6]* ) # Generate SSL Certificates using Lets Encrypt
         if [ -z "$fqdn" ]; then getFQDN; fi
         ./includes/linux/letsencrypt.sh $fqdn;;
-      [7]*) # Enable Lets Encrypt Automatic Renewal
+      [7]* ) # Enable Lets Encrypt Automatic Renewal
         ./includes/linux/le-autorenew.sh;;
-      [8]*) # Create httpd vHosts
+      [8]* ) # Create httpd vHosts
         if [ -z "$fqdn" ]; then getFQDN; fi
         ./includes/linux/apache_add_vhost.sh;;
-      [9]*) # create nodemanager service
+      [9]* ) # create nodemanager service
         ./includes/nodemanager_service.sh;;
-      [10]*) # Start adminserver using nodemanager
+      [10]* ) # Start adminserver using nodemanager
         ./includes/oracle/nodemanager.sh ;;
-      [q]*) echo "Exiting"; break;;
+      [q]* ) echo "Exiting"; break;;
     esac
 done
