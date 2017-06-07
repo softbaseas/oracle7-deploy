e7414da758142d5e18633b16a78f2bdd8517b8ae
Anders W. Olsen <anders@wiberg.tech>
2017-06-07 14:05:52 +0200

    change config.xml as well

--- includes/oracle/formsweb.sh
+++ includes/oracle/formsweb.sh
@@ -5,4 +5,8 @@ sed "s/changefmw.softbase.dk/$fqdn/" /home/oracle/Oracle/Middleware12c/ofr1/user
 mv /home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12/config/fmwconfig/servers/WLS_FORMS/applications/formsapp_12.2.1/config/formsweb.cfg /home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12/config/fmwconfig/servers/WLS_FORMS/applications/formsapp_12.2.1/config/formsweb.cfg.original
 mv ./new_formsweb.cfg /home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12/config/fmwconfig/servers/WLS_FORMS/applications/formsapp_12.2.1/config/formsweb.cfg
 
+sed "s/changefmw.softbase.dk/$fqdn/" /home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12/config/config.xml > ./new_config.xml
+mv /home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12/config/config.xml /home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12/config/config.xml.original
+mv ./new_config.xml /home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12/config/config.xml
+
 echo "Changes made in formsweb."
